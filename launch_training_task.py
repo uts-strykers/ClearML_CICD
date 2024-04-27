57e8ba5b3ac18d2b3286fc392f609f5c55d5fc83
GitarthVaishnav <gitarthv@outlook.com>
2024-04-27 16:45:28 +0530

    Git Commit

--- launch_training_task.py
+++ launch_training_task.py
@@ -3,11 +3,11 @@ from clearml import Task
 from train_model import train_and_evaluate
 
 
-def launch_training_task(repo, branch):
+def launch_training_task(repo, branch, commit):
     task = Task.init(
         project_name="My ML Project", task_name="Model Training on Latest Script"
     )
-    task.set_repo(repo=repo, branch=branch)
+    task.set_repo(repo=repo, branch=branch, commit=commit)
     # Print the task ID with a unique prefix
     print(f"TASK_ID_OUTPUT: {task.id}")
     task.execute_remotely(queue_name="gitarth", exit_process=True)
