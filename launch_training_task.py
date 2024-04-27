7dde341fa460e8be1aba77f1de30a6399ad076a1
gitarthvaishnav <gitarth.vaishnav@droneshield.com>
2024-04-28 09:36:18 +1000

    updated code

--- launch_training_task.py
+++ launch_training_task.py
@@ -1,20 +1,20 @@
-import sys
+import argparse
 from clearml import Task
 from train_model import train_and_evaluate
 
-
-def launch_training_task(repo_url, branch_name, commit_hash):
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Launch a ClearML task for training a model.")
+    parser.add_argument("repo_url", type=str, help="URL of the repository containing the training script")
+    parser.add_argument("branch_name", type=str, help="Branch name of the repository")
+    parser.add_argument("commit_hash", type=str, help="Commit hash for the training script version")
+    
+    args = parser.parse_args()
+    
     task = Task.init(
         project_name="My ML Project", task_name="Model Training on Latest Script"
     )
+    task.connect_args(args)
     # Print the task ID in a format that can be easily captured
     print(f"TASK_ID_OUTPUT: {task.id}")
-    task.set_repo(repo=repo_url, branch=branch_name, commit=commit_hash)
+    task.set_repo(repo=args.repo_url, branch=args.branch_name, commit=args.commit_hash)
     task.execute_remotely(queue_name="gitarth", exit_process=True)
-
-
-if __name__ == "__main__":
-    repo_url = sys.argv[1]
-    branch_name = sys.argv[2]
-    commit_hash = sys.argv[3]
-    launch_training_task(repo_url, branch_name, commit_hash)
