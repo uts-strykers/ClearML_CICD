e4ca7adb8e8f5b963134ca17cf9741893b574c42
GitarthVaishnav <gitarthv@outlook.com>
2024-04-27 16:26:48 +0530

    update

--- launch_training_task.py
+++ launch_training_task.py
@@ -6,11 +6,10 @@ def launch_training_task():
     task = Task.init(
         project_name="My ML Project", task_name="Model Training on Latest Script"
     )
-    task.execute_remotely(queue_name="gitarth", exit_process=True)
-    train_and_evaluate(task)
-
     # Print the task ID with a unique prefix
     print(f"TASK_ID_OUTPUT: {task.id}")
+    task.execute_remotely(queue_name="gitarth", exit_process=True)
+    train_and_evaluate(task)
 
 
 if __name__ == "__main__":
