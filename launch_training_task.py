c60700e5157dacc3439d3fffdc0e40df506caaef
GitarthVaishnav <gitarthv@outlook.com>
2024-04-27 16:36:34 +0530

    updated

--- launch_training_task.py
+++ launch_training_task.py
@@ -1,11 +1,13 @@
+import sys
 from clearml import Task
 from train_model import train_and_evaluate
 
 
-def launch_training_task():
+def launch_training_task(repo, branch):
     task = Task.init(
         project_name="My ML Project", task_name="Model Training on Latest Script"
     )
+    task.set_repo(repo=repo, branch=branch)
     # Print the task ID with a unique prefix
     print(f"TASK_ID_OUTPUT: {task.id}")
     task.execute_remotely(queue_name="gitarth", exit_process=True)
@@ -13,4 +15,7 @@ def launch_training_task():
 
 
 if __name__ == "__main__":
-    launch_training_task()
+    # Arguments order: repository URL, branch name
+    repo_arg = sys.argv[1] if len(sys.argv) > 1 else None
+    branch_arg = sys.argv[2] if len(sys.argv) > 2 else None
+    launch_training_task(repo_arg, branch_arg)
