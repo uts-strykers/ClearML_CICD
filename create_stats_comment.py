9cd4ac0766073844e6a68241d6b90cd3c092bb5f
gitarthvaishnav <gitarth.vaishnav@droneshield.com>
2024-04-28 09:53:26 +1000

    updated

--- create_stats_comment.py
+++ create_stats_comment.py
@@ -3,14 +3,13 @@ from clearml import Task
 from github3 import login
 import sys
 
-
 def post_pr_comment(task_id):
     task = Task.get_task(task_id=task_id)
     github = login(token=os.getenv("GH_TOKEN"))
     repo_name = os.getenv("GITHUB_REPOSITORY")
-    pr_number = os.getenv("GITHUB_REF").split("/")[-1]
+    pr_number = os.getenv("PR_NUMBER")  # Get PR number set in the workflow
     repo = github.repository(*repo_name.split("/"))
-    pr = repo.pull_request(int(pr_number))
+    pr = repo.pull_request(int(pr_number))  # Convert string PR number to int
 
     metrics = task.get_last_scalar_metrics()
     comment_body = "### Model Evaluation Results\n\n"
@@ -19,7 +18,6 @@ def post_pr_comment(task_id):
             comment_body += f"**{title} - {ser}:** Last: {val['last']}\n"
 
     pr.create_issue_comment(comment_body)
-
 
 if __name__ == "__main__":
     task_id = sys.argv[1]  # Task ID needs to be passed as an argument
